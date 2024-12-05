0dae3840e6543275c854a9cc2c07e944e3895427
Vincent Junior <vincent_cars@yahoo.com>
2024-12-05 16:19:10 +0700

    finish soal test

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
-
 // return maximum profit by day
 func MaximalProfitStock(stock []int) int {
-	minPrice := int(^uint(0) >> 1)
-	maxProfit := 0
-	return maxProfit
+
+}
+
+func AdditionEven(even []int) int {
+
+}
+
+func MaximumAndMinimumNumber(number []int) (min, max int) {
+
 }
 
 func main() {
-	fmt.Println(MaximalProfitStock([]int{2, 5, 6, 1, 3}))
-	fmt.Println(MaximalProfitStock([]int{10, 2, 1, 4, 6}))
-	fmt.Println(MaximalProfitStock([]int{8, 2, 5, 1, 5, 7}))
+
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMaximumProfitStock(t *testing.T) {
 	test := []struct {
@@ -16,6 +18,51 @@ func TestMaximumProfitStock(t *testing.T) {
 		t.Run("", func(t *testing.T) {
 			result := MaximalProfitStock(tt.TestCase)
 			if result != tt.Expected {
+				t.Errorf("Failed running testcase %v", tt.TestCase)
+			} else {
+				t.Logf("Test case %d Success", i)
+			}
+		})
+	}
+}
+
+func TestAdditionEvent(t *testing.T) {
+	test := []struct {
+		TestCase []int
+		Expected int
+	}{
+		{[]int{1, 10, 13, 2, 41, 55, 20}, 32},
+		{[]int{1, 21, 22, 23, 24, 25, 26}, 72},
+		{[]int{1022, 1021, 1023, 2041, 2341}, 1022},
+	}
+
+	for i, tt := range test {
+		t.Run("", func(t *testing.T) {
+			result := AdditionEven(tt.TestCase)
+			if result != tt.Expected {
+				t.Error(result)
+				t.Errorf("Failed running testcase %v", tt.TestCase)
+			} else {
+				t.Logf("Test case %d Success", i)
+			}
+		})
+	}
+}
+
+func TestMaximumAndMinimumNumber(t *testing.T) {
+	test := []struct {
+		TestCase []int
+		Expected []int
+	}{
+		{[]int{1, 9, 10, 2, 5, 76, 2, 4}, []int{1, 76}},
+		{[]int{502, 201, 222, 455, 92, 104}, []int{104, 502}},
+		{[]int{1022, 1021, 1023, 2041, 2341}, []int{1021, 2341}},
+	}
+
+	for i, tt := range test {
+		t.Run("", func(t *testing.T) {
+			min, max := MaximumAndMinimumNumber(tt.TestCase)
+			if min != tt.Expected[0] && max != tt.Expected[1] {
 				t.Errorf("Failed running testcase %v", tt.TestCase)
 			} else {
 				t.Logf("Test case %d Success", i)
